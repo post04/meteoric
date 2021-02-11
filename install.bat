f1b7fa846fe49ac6a70d8ea93ece05a24cfc1495
Azael <53409082+azaelgg@users.noreply.github.com>
2021-02-11 22:03:10 +0100

    Create install.bat

---
+++ install.bat
@@ -0,0 +1,10 @@
+title meteoric (installing requirements)
+
+go get github.com/valyala/fasthttp 
+go get github.com/Jeffail/gabs 
+go get github.com/gorilla/websocket 
+go get github.com/gookit/color 
+go get gopkg.in/yaml.v2 
+go get gopkg.in/mgo.v2/bson
+
+cls
