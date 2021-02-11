3276609168d1f888e68a4edb53b50781948aa65c
Azael <53409082+azaelgg@users.noreply.github.com>
2021-02-11 21:35:50 +0100

    Update message.go

--- discord/message.go
+++ discord/message.go
@@ -1,1 +1,20 @@
 package discord
+
+import "time"
+
+// MessageCreate event struct
+type MessageCreate struct {
+	ChannelID       string    `json:"channel_id"`
+	Content         string    `json:"content"`
+	GuildID         string    `json:"guild_id"`
+	ID              string    `json:"id"`
+	Nonce           string    `json:"nonce"`
+	Author          Author    `json:"author"`
+	Member          Member    `json:"member"`
+	Timestamp       time.Time `json:"timestamp"`
+	MentionEveryone bool      `json:"mention_everyone"`
+	Pinned          bool      `json:"pinned"`
+	Tts             bool      `json:"tts"`
+	Flags           int       `json:"flags"`
+	Type            int       `json:"type"`
+}
