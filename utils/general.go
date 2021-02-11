f4eff05cca1358fa452dd0f36f8874f727b584b8
Azael <53409082+azaelgg@users.noreply.github.com>
2021-02-11 21:28:58 +0100

    Update general.go

--- utils/general.go
+++ utils/general.go
@@ -1,6 +1,6 @@
 package utils
 
-// Find a value inside the specific list.
+// Find a value inside the specified list.
 func Find(list []string, value string) bool {
 	for _, item := range list {
 		if item == value {
