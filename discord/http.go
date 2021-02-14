54d422e4906d90af8138879c233ef21e2489abea
post <61803796+postrequest69@users.noreply.github.com>
2021-02-14 18:20:15 -0500

    Update http.go

--- discord/http.go
+++ discord/http.go
@@ -12,7 +12,7 @@ var (
 )
 
 // BuildRequest builds a fasthttp request and returns request and response.
-func BuildRequest(Method string, URL string, Body []byte) (req *fasthttp.Request, resp *fasthttp.Response) {
+func BuildRequest(Method, URL string, Body []byte) (req *fasthttp.Request, resp *fasthttp.Response) {
 	req = fasthttp.AcquireRequest()
 	req.Header.SetMethod(Method)
 	req.SetRequestURI(URL)
@@ -22,12 +22,28 @@ func BuildRequest(Method string, URL string, Body []byte) (req *fasthttp.Request
 }
 
 // ClaimCode claims a discord gift code and returns http response.
-func (s *Session) ClaimCode(Code string, ChannelID string) (body *fasthttp.Response, err error) {
+func (s *Session) ClaimCode(Code, ChannelID, Token string) (body *fasthttp.Response, err error) {
 	req, resp := BuildRequest("POST", fmt.Sprintf(baseURL+"/entitlements/gift-codes/%v/redeem", Code), []byte(fmt.Sprintf(`{"channel_id": "%v"}`, ChannelID)))
 	req.Header.SetContentType("application/json")
-	req.Header.Set("authorization", s.Token)
+	req.Header.Set("authorization", Token)
 	if err := fasthttp.Do(req, resp); err == nil {
 		return resp, nil
 	}
 	return nil, errors.New("claimer: error claiming nitro: " + err.Error())
+}
+
+// CheckToken - checks to make sure the token claiming the nitro is a valid token.
+func CheckToken(token string) bool {
+	req := fasthttp.AcquireRequest()
+	req.Header.SetMethod("GET")
+	req.SetRequestURI("https://discordapp.com/api/v7/users/@me")
+	req.Header.Set("Authorization", token)
+	resp := fasthttp.AcquireResponse()
+	if err := fasthttp.Do(req, resp); err != nil {
+		return false
+	}
+	if resp.StatusCode() == 200 {
+		return true
+	}
+	return false
 }
